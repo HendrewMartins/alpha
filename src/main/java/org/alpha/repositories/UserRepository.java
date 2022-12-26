13dc8cb8c185927d52861710b5e90d4902c2c731
Andre Zanlorensi <andrezanlorensi@hotmail.com>
2022-12-26 14:30:45 -0300

    Aqui no repository eu basicamente escrevi que o nome pode ser encontrado digitando qualquer parte do nome

--- src/main/java/org/alpha/repositories/UserRepository.java
+++ src/main/java/org/alpha/repositories/UserRepository.java
@@ -7,12 +7,15 @@ import javax.enterprise.context.ApplicationScoped;
 import org.alpha.entities.User;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
 
-    public List<User> FindByUserName(String user_name) {
-        return find("user_name", user_name).list();
+    public List<User> FindByUserName(String user_name) { //the name part will get this parameter
+        return find("lower(user_name) like lower (:name)",Parameters.with("name", "%"+user_name+"%")).list();
+        //with(name) is my parameter it will be replaced by user_name with is my parameter
+        //"%"+user_name+"%" it means that any part of the name will be found
         // o user_name gonna receive nome and nome receive nome as a value
         //its useful when i need to use it in a ilike script for example
         
